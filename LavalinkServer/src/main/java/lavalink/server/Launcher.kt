c25a388c0906291a71b01ba714ae1f5a84563c2a
PredaaA <46051820+PredaaA@users.noreply.github.com>
2020-07-21 15:16:26 +0200

    bruh I forgot build number

--- LavalinkServer/src/main/java/lavalink/server/Launcher.kt
+++ LavalinkServer/src/main/java/lavalink/server/Launcher.kt
@@ -70,7 +70,7 @@ object Launcher {
             }
             appendln()
             append("${indentation}Version:        "); appendln(version)
-            append("${indentation}Build:          "); appendln("1075-PH-MANAGER")
+            append("${indentation}Build:          "); appendln("1081-PH-MANAGER")
             if (gitRepoState.isLoaded) {
                 append("${indentation}Build time:     "); appendln(buildTime)
                 append("${indentation}Branch          "); appendln(gitRepoState.branch)
