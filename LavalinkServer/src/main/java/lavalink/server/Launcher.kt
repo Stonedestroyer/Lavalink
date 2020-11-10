774088f19a572dec2928124cb020f496f5fd01ba
PredaaA <46051820+PredaaA@users.noreply.github.com>
2020-11-10 01:44:11 +0100

    Bump version.

--- LavalinkServer/src/main/java/lavalink/server/Launcher.kt
+++ LavalinkServer/src/main/java/lavalink/server/Launcher.kt
@@ -70,7 +70,7 @@ object Launcher {
             }
             appendln()
             append("${indentation}Version:        "); appendln(version)
-            append("${indentation}Build:          "); appendln("1108-PH-MANAGER-LP-1.3.60-DEVO")
+            append("${indentation}Build:          "); appendln("1142-PH-MANAGER")
             if (gitRepoState.isLoaded) {
                 append("${indentation}Build time:     "); appendln(buildTime)
                 append("${indentation}Branch          "); appendln(gitRepoState.branch)
