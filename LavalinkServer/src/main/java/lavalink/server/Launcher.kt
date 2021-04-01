2572dae57b9bba914e4fc9fd59e3da67a746f4b1
PredaaA <46051820+PredaaA@users.noreply.github.com>
2021-04-01 17:22:50 +0200

    Use my LP fork temporary to include null NPEs fix.

--- LavalinkServer/src/main/java/lavalink/server/Launcher.kt
+++ LavalinkServer/src/main/java/lavalink/server/Launcher.kt
@@ -70,7 +70,7 @@ object Launcher {
             }
             appendln()
             append("${indentation}Version:        "); appendln(version)
-            append("${indentation}Build:          "); appendln("1215-PH-MANAGER")
+            append("${indentation}Build:          "); appendln("1215-PHMANAGER-LP#604")
             if (gitRepoState.isLoaded) {
                 append("${indentation}Build time:     "); appendln(buildTime)
                 append("${indentation}Branch          "); appendln(gitRepoState.branch)
