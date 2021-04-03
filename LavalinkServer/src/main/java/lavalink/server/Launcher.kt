264779c157f3a08bbf54c81f89388892ea2da5f9
PredaaA <46051820+PredaaA@users.noreply.github.com>
2021-04-03 21:50:46 +0200

    Bump build.

--- LavalinkServer/src/main/java/lavalink/server/Launcher.kt
+++ LavalinkServer/src/main/java/lavalink/server/Launcher.kt
@@ -70,7 +70,7 @@ object Launcher {
             }
             appendln()
             append("${indentation}Version:        "); appendln(version)
-            append("${indentation}Build:          "); appendln("1215-PHMANAGER-LP#604-LP#605")
+            append("${indentation}Build:          "); appendln("1219-PHMANAGER")
             if (gitRepoState.isLoaded) {
                 append("${indentation}Build time:     "); appendln(buildTime)
                 append("${indentation}Branch          "); appendln(gitRepoState.branch)
